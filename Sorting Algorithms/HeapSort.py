9af90a58560d9e938dcc5033367318d34ae52233
PULA V LAKSHMI NARASIMHA NAIDU <117836213+pvln94@users.noreply.github.com>
2024-08-25 16:25:52 +0530

    Update HeapSort.py

--- Sorting Algorithms/HeapSort.py
+++ Sorting Algorithms/HeapSort.py
@@ -1,56 +1,28 @@
-# Python program for implementation of heap Sort
+def heapify(a,n,i):
+    lar = i
+    l = 2*i + 1
+    r = 2*i + 2
+    
+    if l<n and a[i] < a[l]:
+        lar = l 
+    if r<n and a[lar]<a[r]:
+        lar = r 
+    if lar!=i:
+        (a[i],a[lar]) = (a[lar],a[i])
+        heapify(a,n,lar)
+        
 
-# To heapify subtree rooted at index i.
-# n is size of heap
-
-
-def heapify(arr, N, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1     # left = 2*i + 1
-    r = 2 * i + 2     # right = 2*i + 2
-
-    # See if left child of root exists and is
-    # greater than root
-    if l < N and arr[largest] < arr[l]:
-        largest = l
-
-    # See if right child of root exists and is
-    # greater than root
-    if r < N and arr[largest] < arr[r]:
-        largest = r
-
-    # Change root, if needed
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  # swap
-
-        # Heapify the root.
-        heapify(arr, N, largest)
-
-# The main function to sort an array of given size
+def h(a):
+    n = len(a)
+    for i in range(n//2-1,-1,-1):
+        heapify(a,n,i)
+    
+    for i in range(n-1,0,-1):
+        (a[i],a[0]) = (a[0],a[i])
+        heapify(a,i,0)
 
 
-def heapSort(arr):
-    N = len(arr)
-
-    # Build a maxheap.
-    for i in range(N//2 - 1, -1, -1):
-        heapify(arr, N, i)
-
-    # One by one extract elements
-    for i in range(N-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
-        heapify(arr, i, 0)
-
-
-# Driver's code
-if __name__ == '__main__':
-    arr = [12, 11, 13, 5, 6, 7]
-
-    # Function call
-    heapSort(arr)
-    N = len(arr)
-
-    print("Sorted array is")
-    for i in range(N):
-        print("%d" % arr[i], end=" ")
-
+a = [1,12,9,5,6,10]
+h(a)
+for i in range(len(a)):
+    print(a[i],end=" ")    
